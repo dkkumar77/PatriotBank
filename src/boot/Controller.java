bf6bbf07b3bb738a3bb1f9d64406260371ef9a49
dkkumar77 <61252338+dkkumar77@users.noreply.github.com>
2021-09-14 12:58:58 -0400

    First push onto github

---
+++ src/boot/Controller.java
@@ -0,0 +1,11 @@
+package boot;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class Controller {
+
+}
--- src/boot/Main.java
+++ src/boot/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package boot;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("bootscene.fxml"));
+        primaryStage.setTitle("Patriot Bank V1");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.setMaxHeight(500);
+        primaryStage.setMinHeight(500);
+        primaryStage.setMaxWidth(800);
+        primaryStage.setMinWidth(800);
+
         primaryStage.show();
     }
 
