6669cb504555cbc3c08a0eab21bd9f95a5cac257
Deepak Kumar <bcs.deepakkumar@gmail.com>
2021-11-03 19:09:26 -0400

    worked on adding more to create a account

--- src/boot/Controllers/createController.java
+++ src/boot/Controllers/createController.java
@@ -2,6 +2,7 @@ package boot.Controllers;
 
 import boot.Model.PathModel;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
@@ -25,7 +26,8 @@ public class createController {
     private JFXButton submitButton;
 
     @FXML
-    private JFXTextField userField;
+    private JFXTextField userField, firstName, lastName;
+
 
     @FXML
     private JFXTextField emailField;
@@ -33,6 +35,8 @@ public class createController {
     @FXML
     private JFXPasswordField passField, passField2;
 
+    @FXML
+    private JFXDatePicker dob;
 
     private Parent root;
     private Stage stage;
@@ -59,25 +63,54 @@ public class createController {
     @FXML
     void handleSubmit(ActionEvent event) throws IOException {
 
-        if(event.getSource().equals(submitButton)) {
-
-            String [] data = getData();
+        if (event.getSource().equals(submitButton)) {
 
 
-            if(emailVerifyer(data[1])) {
-                if (matchingPasswords(data[2], data[3])) {
-                    FXMLLoader loader = new FXMLLoader(getClass().getResource(PathModel.VerificationScene));
-                    root = loader.load();
-                    verificationController controller = loader.getController();
-                    controller.displayVerificationText(email);
-                    stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                    scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
+
+            String[] data = getData();
+
+            if(emptyCheck()) {
+                if (usernameVerifyer(data[0])) {
+                    if (emailVerifyer(data[1])) {
+                        if (matchingPasswords(data[2], data[3])) {
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource(PathModel.VerificationScene));
+                            root = loader.load();
+                            verificationController controller = loader.getController();
+                            controller.displayVerificationText(email);
+                            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                            scene = new Scene(root);
+                            stage.setScene(scene);
+                            stage.show();
+                        }
+                    }
+
                 }
+
             }
 
+
         }
+    }
+
+    private boolean emptyCheck() {
+        if(password.isEmpty() || password_verifyer.isEmpty() || firstName.getText().isEmpty() || lastName.getText().isEmpty() || emailField.getText().isEmpty() || userField.getText().isEmpty() || dob.getValue().toString().isEmpty()){
+            return false;
+
+        }
+        return true;
+
+    }
+
+
+    private boolean usernameVerifyer(String a){
+        if(username.matches("^[a-zA-Z0-9]*$")){
+
+            return true;
+
+        }
+
+        return false;
+
 
 
     }
